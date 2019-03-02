dee1af9bd91f4b9af5fac0973c7cfced3c4c13b3
Zaynaib Giwa <zaynaibg@gmail.com>
2019-03-01 20:59:42 -0600

    create descritive variable names.

--- marvel_api.py
+++ marvel_api.py
@@ -1,15 +1,9 @@
 import requests
 
-r =requests.get('https://github.com/zaynaib')
-print('This response is from github',r.status_code)
+response =requests.get('https://github.com/zaynaib')
+print('This response is from github', response.status_code)
 
-
-
-'''
-import matplotlib.pyplot as plt
-import numpy as np
-
-x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
-plt.plot(x, np.sin(x))       # Plot the sine of each x point
-plt.show()                   # Display the 
-'''
+if response.status_code == 200:
+    print('Success!')
+elif response.status_code == 404:
+    print('Not Found.')
