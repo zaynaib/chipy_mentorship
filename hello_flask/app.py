25c6366cf61638bce743af081eb6dff0ca287d01
Zaynaib Giwa <zaynaibg@gmail.com>
2019-03-04 21:55:57 -0600

    trying to render html templates

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -1,12 +1,24 @@
 #https://code.visualstudio.com/docs/python/tutorial-flask
 
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 @app.route('/<name>')
 def index(name="Treehouse"):
     return "Hello, from {}!".format(name)
+
+@app.route('/add/<int:num1>/<int:num2>')
+@app.route('/add/<float:num1>/<float:num2>')
+@app.route('/add/<int:num1>/<float:num2>')
+@app.route('/add/<float:num1>/<int:num2>')
+def add(num1, num2):
+    return render_template("add.html")
+    #return '{} + {} = {}'.format(num1,num2,num1+num2)
+
+
+
 
 app.run(debug=True)
 
