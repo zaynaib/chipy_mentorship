081ab946ce4b2a7d2523d754a0ba22d0ab913a04
Zaynaib Giwa <zaynaibg@gmail.com>
2019-03-05 15:26:54 -0600

    pass variables from control to view in flask

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -7,14 +7,15 @@ app = Flask(__name__)
 @app.route("/")
 @app.route('/<name>')
 def index(name="Treehouse"):
-    return "Hello, from {}!".format(name)
+    return render_template("index.html", name = name)
+    #return "Hello, from {}!".format(name)
 
 @app.route('/add/<int:num1>/<int:num2>')
 @app.route('/add/<float:num1>/<float:num2>')
 @app.route('/add/<int:num1>/<float:num2>')
 @app.route('/add/<float:num1>/<int:num2>')
 def add(num1, num2):
-    return render_template("add.html")
+    return render_template("add.html", num1= num1, num2=num2)
     #return '{} + {} = {}'.format(num1,num2,num1+num2)
 
 
