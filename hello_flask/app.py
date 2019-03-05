b6121159c9dec6b1d5f7e3161b8468cbc347fd40
Zaynaib Giwa <zaynaibg@gmail.com>
2019-03-04 20:33:20 -0600

    changed debug configuration so debugging can run

--- hello_flask/app.py
+++ hello_flask/app.py
@@ -1,12 +1,11 @@
 #https://code.visualstudio.com/docs/python/tutorial-flask
 
 from flask import Flask
-from flask import request
 app = Flask(__name__)
 
 @app.route("/")
+@app.route('/<name>')
 def index(name="Treehouse"):
-    name = request.args.get('name',name)
     return "Hello, from {}!".format(name)
 
 app.run(debug=True)
