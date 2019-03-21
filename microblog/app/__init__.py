cebc7da5df22e604a7d25c5a832a939ad78c5268
Zaynaib Giwa <zaynaibg@gmail.com>
2019-03-20 22:43:39 -0500

    flask microblog database

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.debug=True
 
-from app import routes
+##import the config instance in or flask app
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+
+from app import routes, models
+
+if __name__ == '__main__':
+    app.debug=True
+
