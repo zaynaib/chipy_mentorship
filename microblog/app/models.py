71918808c3142179ade0b1c2190321b065db7ff5
Zaynaib Giwa <zaynaibg@gmail.com>
2019-03-26 14:21:17 -0500

    Flask mega tutorial chapter 5 completed

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,7 +1,10 @@
 from datetime import datetime
-from app import db
+from app import db,login
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from hashlib import md5
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index = True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -10,6 +13,23 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    def set_password(self,password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self,password):
+        return check_password_hash(self.password_hash,password)
+
+    
+    def avatar(self,size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest,size)
+
+#load a unique login user
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -19,3 +39,4 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
